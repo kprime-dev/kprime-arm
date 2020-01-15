c14484445235d9fd2441fae84741f5e8a53902ed
npedot <nicola.pedot@gmail.com>
2020-01-15 12:01:15 +0100

    adds detect apply signatures to person vsplit scenario

--- src/main/kotlin/unibz.cs.semint.kprime/scenario/PersonVSplitScenario.kt
+++ src/main/kotlin/unibz.cs.semint.kprime/scenario/PersonVSplitScenario.kt
@@ -1,25 +1,23 @@
 package unibz.cs.semint.kprime.scenario
 
 import unibz.cs.semint.kprime.adapter.service.XMLSerializerJacksonAdapter
-import unibz.cs.semint.kprime.domain.Column
-import unibz.cs.semint.kprime.domain.Constraint
-import unibz.cs.semint.kprime.domain.Database
-import unibz.cs.semint.kprime.domain.Table
+import unibz.cs.semint.kprime.domain.*
 import unibz.cs.semint.kprime.usecase.XMLSerializeUseCase
+import javax.xml.crypto.Data
 
 class PersonVSplitScenario {
 
     fun run() {
         val personMetadata = buildPersonMetadata()
         print(XMLSerializeUseCase(XMLSerializerJacksonAdapter()).prettyDatabase(personMetadata).ok)
-        //vsplitSakila(personMetadata)
+        vsplitSakila(personMetadata)
 
     }
 
     private fun vsplitSakila(personMetadata: Database) {
-        TODO("not implemented")
-        // if detect(db):result
-        //  apply(db,result):db
+        val  detected = detect(personMetadata)
+        if (detected.ok!=null)
+            apply(personMetadata,detected)
     }
 
     private fun buildPersonMetadata(): Database {
@@ -39,6 +37,14 @@ class PersonVSplitScenario {
         primaryConstraint.source.columns.add(colT)
         db.schema.constraints.add(primaryConstraint)
         return db
+    }
+
+    private fun detect(personMetadata: Database): UseCaseResult<Database> {
+        return UseCaseResult("done detect",personMetadata)
+    }
+
+    private fun apply(personMetadata: Database, detected: UseCaseResult<Database>): UseCaseResult<Database>{
+        return UseCaseResult("done apply", personMetadata)
     }
 
 }
