e73ef062e9eba84add742b6cc0906000961bd4a2
npedot <nicola.pedot@gmail.com>
2022-12-15 17:48:52 +0100

    updated xstream version

--- src/test/kotlin/unibz/cs/semint/kprime/usecase/serialize/XStreamTest.kt
+++ src/test/kotlin/unibz/cs/semint/kprime/usecase/serialize/XStreamTest.kt
@@ -51,6 +51,14 @@ class XStreamTest {
         // when
         val xstream = XStream()
         xstream.alias("database",Database().javaClass)
+//        xstream .addPermission(NoTypePermission.NONE); //forbid everything
+//        xstream .addPermission(NullPermission.NULL);   // allow "null"
+//        xstream .addPermission(PrimitiveTypePermission.PRIMITIVES); // allow primitive types
+        xstream.allowTypesByWildcard(
+            arrayOf(
+                "unibz.cs.semint.kprime.**"
+            )
+        )
         val db: Database = xstream.fromXML(dbxml) as Database
         // then
         val prettyDatabase = XMLSerializerJacksonAdapter().prettyDatabase(db)
